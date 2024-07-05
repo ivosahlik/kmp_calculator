3e30bd592e05f4d1d4a749349b58f375bb9c9162
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2024-07-05 23:16:46 +0200

    added view model logic

--- androidApp/src/main/java/cz/ivosahlik/calculator/android/CalculatorState.kt
+++ androidApp/src/main/java/cz/ivosahlik/calculator/android/CalculatorState.kt
@@ -3,5 +3,5 @@ package cz.ivosahlik.calculator.android
 data class CalculatorState(
     val firstOperand : String = "0",
     val currentOperand : String = "0",
-    val operator : String = "0",
+    val operator : String = "",
 )
--- androidApp/src/main/java/cz/ivosahlik/calculator/android/CalculatorViewModel.kt
+++ androidApp/src/main/java/cz/ivosahlik/calculator/android/CalculatorViewModel.kt
@@ -2,6 +2,7 @@ package cz.ivosahlik.calculator.android
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class CalculatorViewModel {
     private val _state = MutableStateFlow(CalculatorState())
@@ -9,12 +10,57 @@ class CalculatorViewModel {
 
     fun onOperatorCLick(button: String) {
         when (button) {
-            "AC" -> { /* TODO: reset the state */ }
-            "+/-" -> { /* TODO: plus/minus opt*/ }
-            "+", "-", "/", "%", "*" -> { }
-            "." -> { }
-            "=" -> { /* TODO: calculate result */ }
+            "AC" -> { _state.update { CalculatorState() } }
+            "+/-" -> { unaryMinus() }
+            "+", "-", "/", "%", "*" -> {
+                _state.update {
+                    it.copy(
+                        firstOperand = it.currentOperand,
+                        operator = button,
+                        currentOperand = "0"
+                    )
+                }
+            }
+            "." -> {
+                addDecimal()
+            }
+            "=" -> {
+                calculateResult()
+            }
             else -> { }
+        }
+    }
+
+    private fun calculateResult() {
+        val first = _state.value.firstOperand.toDoubleOrNull()
+        val second = _state.value.currentOperand.toDoubleOrNull()
+        if (first == null || second == null) return
+        _state.update {
+            when (it.operator) {
+                "/" -> CalculatorState(currentOperand = if (second == 0.0) "0" else (first/second).toString())
+                "*" -> CalculatorState(currentOperand = (first * second).toString())
+                "+" -> CalculatorState(currentOperand = (first + second).toString())
+                "-" -> CalculatorState(currentOperand = (first - second).toString())
+                "%" -> CalculatorState(currentOperand = (first % second).toString())
+                else -> it
+            }
+        }
+    }
+
+    private fun addDecimal() {
+        _state.update {
+            it.copy(
+                currentOperand = "${it.currentOperand.trimEnd('.')}."
+            )
+        }
+    }
+
+    private fun unaryMinus() {
+        val value = _state.value.currentOperand.toDoubleOrNull() ?: 0.0
+        if (value != 0.0) {
+            _state.update {
+                it.copy(currentOperand = value.unaryMinus().toString())
+            }
         }
     }
 }
